4ceafa38061fcc09c6ba170eb0726c8ff1d7cc34
Kavia AI <production@kavia.ai>
2025-08-13 14:23:30 +0000

    CheckPoint - cg725e80e6

--- notes_frontend/src/app/app.routes.ts
+++ notes_frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { NoteListComponent } from './components/note-list/note-list.component';
+import { NoteEditorComponent } from './components/note-editor/note-editor.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: NoteListComponent },
+  { path: 'new', component: NoteEditorComponent },
+  { path: 'edit/:id', component: NoteEditorComponent },
+];
